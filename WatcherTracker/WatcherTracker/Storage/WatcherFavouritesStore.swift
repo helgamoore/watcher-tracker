89328d85ffa663fe2e3f992fd2f115a4982f7aeb
Helga Moore <helgamoore625@gmail.com>
2026-09-15 15:17:55 +1200

    Loading snapshot bug has been fixed

--- WatcherTracker/WatcherTracker/Storage/WatcherFavouritesStore.swift
+++ WatcherTracker/WatcherTracker/Storage/WatcherFavouritesStore.swift
@@ -11,7 +11,7 @@ struct WatcherFavouritesStore {
 
     private let fileManager = FileManager.default
 
-    private let fileName = "watchers_favourites.txt"
+    private let fileName = "favourites.txt"
 
     func load(from archiveFolder: URL) throws -> Set<String> {
         let url = archiveFolder
